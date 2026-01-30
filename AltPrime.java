8b93244c1398554e2a50ca66306feba2a203d27a
Prajwal-Gowda-N <prajwal.g12305@gmail.com>
2026-01-30 09:49:13 +0530

    Second commit

--- AltPrime.java
+++ AltPrime.java
@@ -3,7 +3,7 @@ public class AltPrime{
        int count = 0;
        for(int n = 2;n<100;n++){
           int i;
-          for(int i = 2;i<n;i++){
+          for(i = 2;i<n;i++){
               if(n%i ==0){
                   break;
 }
